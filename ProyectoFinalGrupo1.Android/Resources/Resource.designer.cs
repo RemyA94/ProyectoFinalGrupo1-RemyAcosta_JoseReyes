42a73ff1fcd608b54a1c09eba0d4e7c6ac879bf4
joseereyes <jreyes@simetricaconsulting.com>
2024-05-16 00:06:59 -0400

    first menu modified

--- ProyectoFinalGrupo1.Android/Resources/Resource.designer.cs
+++ ProyectoFinalGrupo1.Android/Resources/Resource.designer.cs
@@ -20421,16 +20421,25 @@ namespace ProyectoFinalGrupo1.Droid
 			public const int preference_list_divider_material = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int reportesicon = 2131165336;
+			public const int programming = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int test_custom_background = 2131165337;
+			public const int reportesicon = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int tooltip_frame_dark = 2131165338;
+			public const int stackbooks = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_light = 2131165339;
+			public const int stats = 2131165339;
+			
+			// aapt resource value: 0x7F07009C
+			public const int test_custom_background = 2131165340;
+			
+			// aapt resource value: 0x7F07009D
+			public const int tooltip_frame_dark = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int tooltip_frame_light = 2131165342;
 			
 			static Drawable()
 			{
